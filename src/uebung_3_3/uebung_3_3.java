4a82fd6eb943b798cee55f65893cc155317e249c
DoerteGorski <doerte.gorski@nordakademie.de>
2015-10-29 17:05:55 +0100

    Uebung_3_3 bearbeitet

--- src/uebung_3_3/uebung_3_3.java
+++ src/uebung_3_3/uebung_3_3.java
@@ -5,16 +5,17 @@ public class uebung_3_3 {
 	public static void main(String[] args) {
 		
 		
-		double Alter = 65;
-		double GewichtInKilogramm = 86.5;
-		double KoerpergroeﬂeInMeter = 1.70;
-		boolean GeschlechtMaennlich = true; 
+		double Alter = 19;
+		double GewichtInKilogramm = 70;
+		double KoerpergroeﬂeInMeter = 1.76;
+		boolean GeschlechtMaennlich = false; 
 		
 		double QuadratDerKoerpergroeﬂe = KoerpergroeﬂeInMeter * KoerpergroeﬂeInMeter;
 
 		double BMI = GewichtInKilogramm / QuadratDerKoerpergroeﬂe;
 		
-		//Programmierung
+		System.out.println("Ihr BMI betraegt " + BMI);
+		
 		
 		if (GeschlechtMaennlich == true)
 			{ 
@@ -57,12 +58,49 @@ public class uebung_3_3 {
 			
 			}
 			
+		else {if (GeschlechtMaennlich == false)
+		{ 
+		if (Alter <= 24) 	{ if (BMI <= 19)	{System.out.println("Untergewicht");}
+		else 				{ if (BMI >= 24)	{System.out.println("‹bergewicht");}
+		else 								{System.out.println("Normalgewicht");}
+		} 
+		}
+		else {if (Alter <= 34) 	{ if (BMI <= 20)	{System.out.println("Untergewicht");}
+		else 					{ if (BMI >= 25)	{System.out.println("‹bergewicht");}
+		else 									{System.out.println("Normalgewicht");}
+		} 
+		}
+		
+		else {if (Alter <= 44) 	{ if (BMI <= 21)	{System.out.println("Untergewicht");}
+		else 					{ if (BMI >= 26)	{System.out.println("‹bergewicht");}
+		else 									{System.out.println("Normalgewicht");}
+		} 
+		}
+		else {if (Alter <= 54) 	{ if (BMI <= 22)	{System.out.println("Untergewicht");}
+		else 					{ if (BMI >= 27)	{System.out.println("‹bergewicht");}
+		else 									{System.out.println("Normalgewicht");}
+		} 
+		}
+		else {if (Alter <= 64) 	{ if (BMI <= 23)	{System.out.println("Untergewicht");}
+		else 					{ if (BMI >= 28)	{System.out.println("‹bergewicht");}
+		else 									{System.out.println("Normalgewicht");}
+		} 
+		}
+		else {if (Alter > 64) 	{ if (BMI <= 24)	{System.out.println("Untergewicht");}
+		else 					{ if (BMI >= 29)	{System.out.println("‹bergewicht");}
+		else 									{System.out.println("Normalgewicht");}
+		} 
+		}
+		}
+		}
+		}
+		}
+		}
+		
+		}
+		}
 		
 		
-		
-		
-		
-		System.out.println("BMI = " + BMI);
 	}
 
 }
